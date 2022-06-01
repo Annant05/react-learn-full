c8aa1aede14dbb77d70eb12edc8ea4f101a8af9a
Annant05 <annantguptaneema@gmail.com>
2022-06-01 23:21:24 +0000

    Learned about dynamic variables in JSX

--- src/app.js
+++ src/app.js
@@ -1,12 +1,38 @@
-console.log("App.js is running.");
+console.log('App.js is running.');
 
 // JSX - JavaScript XML
+
+var app = {
+    title: 'Indecision App',
+    subtitle: 'Put your life in the hands of a computer',
+}
 var template = (
     <div>
-        <h1>This is Hello JSX from app.js</h1>
-        <p>This is some info</p>
+        <h1>{app.title}</h1>
+        <p>{app.subtitle}</p>
+        <ol>
+            <li>Item one</li>
+            <li>Item two</li>
+        </ol>
     </div>
 );
-var appRoot = document.getElementById("app");
 
+var appRoot = document.getElementById('app');
 ReactDOM.render(template, appRoot);
+
+var user = {
+    name: 'Annant Gupta',
+    age: 24,
+    location: 'Indore',
+}
+
+var templateTwo = (
+    <div>
+        <h1>{user.name}</h1>
+        <p>Age: {user.age}</p>
+        <p>Location: {user.location}</p>
+    </div>
+);
+
+// ReactDOM.render(templateTwo, appRoot);
+
