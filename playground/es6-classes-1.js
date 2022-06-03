1e65caae45e5b8b099127b772f7272547e8b2515
Annant05 <annantguptaneema@gmail.com>
2022-06-03 19:31:57 +0000

    used classed from ES6 and learned about extends

--- playground/es6-classes-1.js
+++ playground/es6-classes-1.js
@@ -15,8 +15,47 @@ class Person {
 
 }
 
-const me = new Person('Annant Gupta', 24);
-console.log(me.getDescription());
+class Student extends Person {
 
-const her = new Person();
-console.log(her.getDescription());
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = super.getDescription();
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}`;
+        }
+        return description;
+    }
+}
+
+class Traveler extends Person {
+
+    constructor(name, age, homeLocation) {
+        super(name, age);
+        this.homeLocation = homeLocation;
+    }
+
+    hasHomeLocation() {
+        return !!this.homeLocation;
+    }
+
+    getGreeting() {
+        let greeting = super.getGreeting();
+        if (this.hasHomeLocation()) {
+            greeting += ` I'm visiting from ${this.homeLocation}.`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Traveler('Annant Gupta', 24, 'Shimla');
+console.log(me.getGreeting());
+
+const her = new Traveler();
+console.log(her.getGreeting());
