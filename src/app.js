a2dda31eb1367392376a9bef833c02becef781be
Annant05 <annantguptaneema@gmail.com>
2022-06-04 17:44:40 +0000

    Learned about passing function as props to update state in the parent component

--- src/app.js
+++ src/app.js
@@ -1,14 +1,33 @@
 class IndecisionApp extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+        this.state = {
+            options: ['Thing one', 'Thing two', 'Thing a']
+        };
+    }
+
+    handleDeleteOptions() {
+        this.setState(() => {
+            return {
+                options: []
+            };
+        });
+    }
+
     render() {
         const title = 'Indecision';
         const subtitle = 'Put your life in the hands of a computer';
-        const options = ['Thing one', 'Thing two', 'Thing three']
 
         return (
             <div>
                 <Header title={title} subtitle={subtitle} />
-                <Action />
-                <Options options={options} />
+                <Action hasOptions={this.state.options.length > 0} />
+                <Options
+                    options={this.state.options}
+                    handleDeleteOptions={this.handleDeleteOptions}
+                />
                 <AddOption />
             </div>
         );
@@ -36,27 +55,20 @@ class Action extends React.Component {
     render() {
         return (
             <div>
-                <button onClick={this.handlePick}>What should i do?</button>
+                <button
+                    onClick={this.handlePick}
+                    disabled={!this.props.hasOptions}
+                >What should i do?</button>
             </div>
         );
     }
 }
 
 class Options extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.handleRemoveAll = this.handleRemoveAll.bind(this);
-    }
-
-    handleRemoveAll() {
-        alert(this.props.options);
-    }
-
     render() {
         return (
             <div>
-                <button onClick={this.handleRemoveAll}>Remove All</button>
+                <button onClick={this.props.handleDeleteOptions}>Remove All</button>
                 {this.props.options.map((option) => (<Option key={option} optionText={option} />))}
             </div>
         );
