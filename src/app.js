2e4dc53d5f69273b30f4b1cda91df9117e11d88d
Annant05 <annantguptaneema@gmail.com>
2022-06-06 18:47:32 +0000

    learned about new class syntax

--- src/app.js
+++ src/app.js
@@ -3,3 +3,31 @@ import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+
+// trying out new class definition type.
+class OldSyntax {
+    constructor() {
+        this.name = 'Mike';
+        this.getGreeting = this.getGreeting.bind(this);
+    }
+    getGreeting() {
+        return `Hi my name is ${this.name}.`;
+    }
+}
+const oldSyntax = new OldSyntax();
+console.log(oldSyntax);
+const getGreeting = oldSyntax.getGreeting;
+console.log(getGreeting());
+
+// example of new type.
+class NewSyntax {
+    name = 'Abc'
+    getGreeting = () => {
+        return `Hi my name is ${this.name}.`;
+    }
+}
+
+const newSyntax = new NewSyntax();
+const getGreeting2 = newSyntax.getGreeting;
+console.log(getGreeting2());
