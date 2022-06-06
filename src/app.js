04e274cb3522110ae6dab701f4da65c8d1004089
Annant05 <annantguptaneema@gmail.com>
2022-06-06 19:00:57 +0000

    using jsx between two component header

--- src/app.js
+++ src/app.js
@@ -2,7 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp'
 
-ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+const Layout = (props) => {
+    return (
+        <div>
+            <p>header </p>
+            {props.children}
+            <p>footer </p>
+        </div>
+    )
+}
+
+const template = (
+    <div>
+        <h1>Page Title</h1>
+        <p>This is my page</p>
+    </div>
+)
+
+ReactDOM.render((
+    <Layout>
+        {template}
+    </Layout>
+), document.getElementById('app'));
 
 
 // // trying out new class definition type.
