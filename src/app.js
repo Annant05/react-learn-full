198dea6ca9b5ab9c116fb1dc931f12ba1d41b663
Annant05 <annantguptaneema@gmail.com>
2022-06-06 17:33:55 +0000

    used default export with example

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
-// import { square, add } from './utils.js';
+// import subtract, { square, add } from './utils.js';
 
 // console.log("app.js is running hello");
 // console.log(square(4));
 // console.log(add(4, 4));
+// console.log(subtract(4, 4));
 
-import { isAdult, canDrink } from './person.js';
+import isSeniorCitizen, { isAdult, canDrink } from './person.js';
 
 console.log(isAdult(21));
 console.log(canDrink(20));
+console.log(isSeniorCitizen(48));
--- src/utils.js
+++ src/utils.js
@@ -5,7 +5,13 @@ console.log('utils.js is running');
 
 //export has two types -> default export and named exports
 // example of named export
-// export { square, add };
 // alternate way for named export.
-// export const square = (x) => x * x;
-// export const add = (a, b) => a + b;
+export const square = (x) => x * x;
+export const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+export default subtract;
+// below is also valid
+// export default (a, b) => a - b;
+
+// export { square, add, subtract as default };
