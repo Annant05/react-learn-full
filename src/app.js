e3c05100010171b594999edded6dbe779243dcb8
Annant05 <annantguptaneema@gmail.com>
2022-06-06 17:19:24 +0000

    learned about import/export syntax of ES6

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
-console.log("app.js is running hello");
+// import { square, add } from './utils.js';
+
+// console.log("app.js is running hello");
+// console.log(square(4));
+// console.log(add(4, 4));
+
+import { isAdult, canDrink } from './person.js';
+
+console.log(isAdult(21));
+console.log(canDrink(20));
---
+++ src/utils.js
@@ -0,0 +1,11 @@
+console.log('utils.js is running');
+
+// const square = (x) => x * x;
+// const add = (a, b) => a + b;
+
+//export has two types -> default export and named exports
+// example of named export
+// export { square, add };
+// alternate way for named export.
+// export const square = (x) => x * x;
+// export const add = (a, b) => a + b;
