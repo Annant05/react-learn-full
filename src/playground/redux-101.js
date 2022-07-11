c147164bd171b1402c8f92ac25447f8391fe20f9
Annant05 <annantguptaneema@gmail.com>
2022-07-11 07:17:45 +0000

    created expenses redux reducer and store

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -21,7 +21,11 @@ const setCount = ({ count } = {}) => ({
     count
 });
 
-const store = createStore((state = { count: 0 }, action) => {
+// Reducers
+// 1. Reducers are pure functions.
+// 2. Never change state or action.
+
+const countReducer = (state = { count: 0 }, action) => {
     switch (action.type) {
         case 'INCREMENT':
             return {
@@ -43,7 +47,9 @@ const store = createStore((state = { count: 0 }, action) => {
             return state;
     }
 
-});
+}
+
+const store = createStore(countReducer);
 
 const unsubscribe = store.subscribe(() => {
     console.log(store.getState());
