642d168eca50b090784baca03a3fa2ab5ad6579a
Annant05 <annantguptaneema@gmail.com>
2022-07-11 09:27:07 +0000

    Added addExpense reducer function

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,4 +1,31 @@
 import { createStore, combineReducers } from 'redux';
+import uuid from 'uuid';
+
+const addExpense = (
+    {
+        description = '',
+        note = '',
+        amount = '',
+        createdAt = 0
+    } = {}
+) => ({
+    type: 'ADD_EXPENSE',
+    expense: {
+        id: uuid(),
+        description,
+        note,
+        amount,
+        createdAt
+    }
+
+});
+
+
+const removeExpense = ({ id } = {}) => ({
+    type: 'REMOVE_EXPENSE',
+    id
+});
+
 
 
 // Expenses Reducer
@@ -6,6 +33,13 @@ const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
     switch (action.type) {
+        case 'ADD_EXPENSE':
+            return [
+                ...state,
+                action.expense
+            ];
+        case 'REMOVE_EXPENSE':
+            return state.filter(({ id }) => id !== action.id)
         default:
             return state;
     }
@@ -33,5 +67,13 @@ const store = createStore(
         filters: filtersReducer
     })
 );
+
+store.subscribe(() => {
+    console.log(store.getState());
+});
+
+store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+store.dispatch(removeExpense({ description: 'Rent', amount: 100 }));
 
 console.log(store.getState());
