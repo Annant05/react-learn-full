00417f3535db1099ed97f6c7b8b6240326752316
Annant05 <annantguptaneema@gmail.com>
2022-07-11 17:31:27 +0000

    completed the expenses reducers with filter and sort

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -126,6 +126,23 @@ const filtersReducer = (state = filtersReducerDefaultState, action) => {
 };
 
 
+// Get visible Expenses
+const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+    return expenses.filter((expense) => {
+        const startDateMatch = typeof startDate !== 'number' || expense.createdAt >= startDate;
+        const endDateMatch = typeof startDate !== 'number' || expense.createdAt <= endDate;
+        const textMatch = expense.description.toLowerCase().includes(text.toLowerCase());
+
+        return startDateMatch && endDateMatch && textMatch;
+    }).sort((a, b) => {
+        if (sortBy === 'date') {
+            return a.createdAt < b.createdAt ? 1 : -1;
+        } else if (sortBy === 'amount') {
+            return a.amount < b.amount ? 1 : -1;
+        }
+    });
+};
+
 const store = createStore(
     combineReducers({
         expenses: expensesReducer,
@@ -134,11 +151,13 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-    console.log(store.getState());
+    const state = store.getState();
+    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
+    console.log(visibleExpenses);
 });
 
-// const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
-// const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 200 }));
+const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 200 }));
 // store.dispatch(removeExpense({ id: expenseOne.expense.id }));
 
 // store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
@@ -148,12 +167,10 @@ store.subscribe(() => {
 // store.dispatch(sortByAmount());
 // store.dispatch(sortByDate());
 
-store.dispatch(setStartDate(125));
-console.log(store.getState());
-store.dispatch(setStartDate());
+// store.dispatch(setStartDate(125));
+// store.dispatch(setStartDate());
 
-store.dispatch(setEndDate(1250));
-console.log(store.getState());
-store.dispatch(setEndDate());
+// store.dispatch(setEndDate(1250));
+// store.dispatch(setEndDate());
 
-console.log(store.getState());
+// console.log(store.getState());
